8bf7847b83a6673344d1b7bd1af343b75646de67
Cainã Brazil <cbr.guaiamum@gmail.com>
2021-11-01 10:33:24 -0300

    Improvements in people usage

--- src/features/people/PeopleList.tsx
+++ src/features/people/PeopleList.tsx
@@ -11,23 +11,33 @@ export const WithAddButton: React.FC<PeopleListProps> = (props) => {
   const inputRef = useRef<HTMLInputElement>();
   const [value, setValue] = useState('')
   const dispatch = useAppDispatch()
+
+  const onAdd = () => {
+    dispatch(add({ id: null, name: inputRef.current.value }))
+    setValue('')
+  };
   return (
     <PeopleList {...props}>
       <input
         type="text"
         placeholder="Enter a name..."
         name="new-person"
-        className="bg-transparent border-b-2"
+        className="bg-transparent border-b mt-auto"
         value={value}
         ref={inputRef}
-        onChange={()=>{
+        onKeyPress={(e) => {
+          if (e.code === 'Enter') {
+            onAdd()
+          }
+        }}
+        onChange={() => {
           setValue(inputRef.current.value)
         }}
       />
       <button
-        className="p-3 font-bold"
+        className="p-4 font-bold"
         onClick={() => {
-          dispatch(add({ id: null, name: inputRef.current.value }));
+          onAdd()
           setValue('')
         }}
       >
@@ -40,14 +50,20 @@ export const WithAddButton: React.FC<PeopleListProps> = (props) => {
 const PeopleList: React.FC<PeopleListProps> = ({ people, blockListIds = [], onPersonClick, sideAddornment, children }) => {
   return (
     <section className="border-t border-opacity-70 border-gray-200 m-2">
-      {people
-        .filter(({ id }) => !blockListIds.includes(id))
-        .map((person) => {
-          return <button key={person.id} className="flex items-center justify-between mt-2 w-full p-2 rounded-md border border-gray-400" onClick={() => onPersonClick(person.id)}>
-            <Person {...person} />
-            {sideAddornment}
-          </button>;
-        })}
+      {
+        people.length ?
+          people
+            .filter(({ id }) => !blockListIds.includes(id))
+            .map((person) => {
+              return <button key={person.id} className="flex items-center justify-between mt-2 w-full p-2 rounded-md border border-gray-400" onClick={() => onPersonClick(person.id)}>
+                <Person {...person} />
+                {sideAddornment}
+              </button>;
+            })
+          :
+          <div className="">
+            No one here :(
+          </div>}
       {children}
     </section>
   );
